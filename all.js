c471b5b9275912876f9c1bc0ecee6efb5c5142ea
Khushhal <235362529+khu47029@users.noreply.github.com>
2026-06-02 08:30:25 +0530

    Add files via upload

--- all.js
+++ all.js
@@ -1,10 +1,9 @@
 import gsap from "./gsap-core.js";
 import CSSPlugin from "./CSSPlugin.js";
-const gsapWithCSS = gsap.registerPlugin(CSSPlugin) || gsap, // to protect from tree shaking
-	TweenMaxWithCSS = gsapWithCSS.core.Tween;
-
+var gsapWithCSS = gsap.registerPlugin(CSSPlugin) || gsap,
+    // to protect from tree shaking
+TweenMaxWithCSS = gsapWithCSS.core.Tween;
 export { gsapWithCSS as gsap, gsapWithCSS as default, TweenMaxWithCSS as TweenMax, CSSPlugin };
-
 export { TweenLite, TimelineMax, TimelineLite, Power0, Power1, Power2, Power3, Power4, Linear, Quad, Cubic, Quart, Quint, Strong, Elastic, Back, SteppedEase, Bounce, Sine, Expo, Circ, wrap, wrapYoyo, distribute, random, snap, normalize, getUnit, clamp, splitColor, toArray, mapRange, pipe, unitize, interpolate, shuffle, selector } from "./gsap-core.js";
 export * from "./CustomEase.js";
 export * from "./Draggable.js";
@@ -18,7 +17,6 @@ export * from "./PixiPlugin.js";
 export * from "./ScrollToPlugin.js";
 export * from "./ScrollTrigger.js";
 export * from "./TextPlugin.js";
-
 export * from "./DrawSVGPlugin.js";
 export * from "./Physics2DPlugin.js";
 export * from "./PhysicsPropsPlugin.js";
